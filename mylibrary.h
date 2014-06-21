9395bb630dd72f99a3bdd7470346ccf95b530786
Christian <slimewire.amil@gmail.com>
2014-06-22 02:53:18 +0800

    Update mylibrary.h

--- mylibrary.h
+++ mylibrary.h
@@ -38,15 +38,15 @@ void initialize()
 unsigned char UARTRead()
 {
 	unsigned char data;
-	while( !(UCSR0A & (1<<RXC0)) ); 	//check if receiving is complete. if there are no data data in the receive buffer,
-                                    //the loop will continue running. This is sometimes called busy-waiting because
-                                    //you are consuming processing power but you are just waiting till you get any input.
-										                //This is also called a blocking statement because the flow of your code is blocked 
-										                //or stopped until you get your input. This means that if you have some important statements
-										                //after this line, they will be delayed. 
+	while( !(UCSR0A & (1<<RXC0)) ); //check if receiving is complete. if there are no data data in the receive buffer,
+                                    	//the loop will continue running. This is sometimes called busy-waiting because
+                                    	//you are consuming processing power but you are just waiting till you get any input.
+					//This is also called a blocking statement because the flow of your code is blocked 
+					//or stopped until you get your input. This means that if you have some important statements
+					//after this line, they will be delayed. 
 	
-	data=UDR0;							          //this lines read the UART data register. Reading this register gives you the content of the  Receive register.
-                                    //you are also required to read this register to clear RXC1.
+	data=UDR0;			//this lines read the UART data register. Reading this register gives you the content of the  Receive register.
+                                    	//you are also required to read this register to clear RXC1.
 										
 	return data;	
 	
@@ -56,5 +56,5 @@ void UARTSend(unsigned char data)
 {
 	while( !( UCSR0A & (1<<UDRE0)) ); 	// same as the first one. But this checks the UART data Register to check if it is empty before writing to it.
 
-	UDR0 = data;                     		// You write the data to UDR if you want to transmit.
+	UDR0 = data;                     	// You write the data to UDR if you want to transmit.
 }
